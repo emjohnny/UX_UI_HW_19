9e62f709ffc1ec471eccaa1c0dea8d1d18dd0fd7
emjohnny <emjohnny343@gmail.com>
2023-08-16 18:29:45 -0500

    update

--- js/index.js
+++ js/index.js
@@ -1,2 +1,24 @@
 console.log("YOU GOT THIS");
+/*var modal = document.getElementById("myModal");
+var btn = document.getElementById("myBtn");
+var span = document.getElementsByClassName("close")[0];
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+span.onclick = function() {
+    modal.style.display = "none";
+}
+window.onclick = function(event) {
+    if (event.target ==modal) {
+        modal.style.display = "none";
+    }
+}*/
+
+$("#mybTn").on("click", function(){
+    $(".modal, .modal-content").addClass("active");
+});
+$(".close", ".modal").on("click", function(){
+    $(".modal", ".modal-content").removeClass("active");
+});
+
 
